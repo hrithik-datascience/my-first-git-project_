03a3c8eb4bec4ffec3c44766a92af964e6dd1966
Hrithik Malviya <Hrithikmalviya5@gmail.com>
2026-05-30 21:55:16 +0530

    Add skill list to hello.py

--- hello.py
+++ hello.py
@@ -4,3 +4,5 @@ course = "MLOps"
 print(f"Hello! My name is {name}.")
 print(f"I am learning {course}.")
 print("This file is tracked by Git.")
+skill = ["Python", "Machine Learning", "Data Science", "MLOps", "Docker", "AWS","Github"]
+print(f"My skills:{', '.join(skill)}")
